18d30d78409b6d239cbc5951392acaba575c795e
Guilherme Duarte <162611293+TurnGui@users.noreply.github.com>
2026-03-18 19:11:45 +0000

    Delete Year1/Programming Labs/Golf Solitaire directory

--- Year1/Programming Labs/Golf Solitaire/include/golf.h
+++
@@ -1,9 +0,0 @@
-#ifndef GOLF_H
-#define GOLF_H
-
-#include "carta.h"
-
-// Verifica se uma carta pode ser jogada segundo as regras do Golf
-int golf_carta_pode_jogar(Carta mesa, Carta baralho);
-
-#endif
