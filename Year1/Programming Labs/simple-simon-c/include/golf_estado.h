8ee7d1a336c4a2acb00ecc9b22c8fa1f6a9830e4
Guilherme Duarte <162611293+TurnGui@users.noreply.github.com>
2026-05-06 16:12:15 +0100

    Add files via upload

---
+++ Year1/Programming Labs/simple-simon-c/include/golf_estado.h
@@ -0,0 +1,33 @@
+#ifndef GOLF_ESTADO_H
+#define GOLF_ESTADO_H
+
+#include "baralho.h"
+#include "carta.h"
+
+#define GOLF_COLUNAS 7
+#define GOLF_LINHAS  5 
+
+typedef struct {
+    Carta mesa[GOLF_COLUNAS][GOLF_LINHAS]; // a mesa
+    int topo[GOLF_COLUNAS];   // -1 se a coluna estiver vazia, senão entre 0 e 4 pq a coluna pode ter ate 5 cartas
+    Carta descarte; // carta no topo do baralho
+    Baralho stock;  // baralho
+} GolfEstado;
+
+void golf_estado_inicializar(GolfEstado *e, unsigned seed);
+
+int golf_stock_vazio(const GolfEstado *e);
+int golf_coluna_vazia(const GolfEstado *e, int coluna);
+
+Carta golf_topo_coluna(const GolfEstado *e, int coluna);
+Carta golf_descarte(const GolfEstado *e);
+
+int golf_pode_jogar_coluna(const GolfEstado *e, int coluna);
+int golf_jogar_coluna(GolfEstado *e, int coluna);
+
+int golf_puxar_stock(GolfEstado *e);
+
+int golf_ganhou(const GolfEstado *e);
+int golf_tem_jogada(const GolfEstado *e);
+
+#endif
